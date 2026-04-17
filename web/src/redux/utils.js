1b4d34e441129f0b855cc661d94d1cdecd53d313
andreivannuabc <157652372+andreivannuabc@users.noreply.github.com>
2026-04-17 12:45:47 -0700

    cambios nginx y utils

--- web/src/redux/utils.js
+++ web/src/redux/utils.js
@@ -1,7 +1,14 @@
-// PRODUCCION
-//export const PATH = "/api";
-//export const WS_PATH = `${window.location.protocol === "https:" ? "wss" : "ws"}://${window.location.host}`;
+// Detectamos si estamos en local o en el servidor
+const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
-// DESARROLLO
-export const PATH = `http://${window.location.hostname}:8000`;
-export const WS_PATH = `ws://${window.location.hostname}:8000`;
+// --- CONFIGURACIÓN DE RUTAS ---
+
+// Si es local, usa el puerto 8000. Si es producción, usa la ruta relativa /api que maneja Nginx.
+export const PATH = isLocal 
+    ? `http://${window.location.hostname}:8000` 
+    : `https://${window.location.hostname}/api`;
+
+// Si es local, usa ws. Si es producción, usa wss y deja que Nginx haga el trabajo en el puerto 443.
+export const WS_PATH = isLocal
+    ? `ws://${window.location.hostname}:8000`
+    : `wss://${window.location.hostname}`;
